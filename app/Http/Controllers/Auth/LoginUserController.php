294504654cd3162c672d60ce3ee324b6856a6df1
rbuzzibarrios <rbuzzibarrios@gmail.com>
2023-05-26 18:57:57 -0400

    define login route at api.php route file
    add api_version key at app.php config file
    
    add auth.php, pagination.php, passwords.php and validation.php lang files
    
    install and configure laravel sail
    
    use user factory to seed users table at DatabaseSeeder.php
    
    use api version config key in RouteServiceProvider.php
    
    use SANCTUM_EXPIRATION_TOKEN key from env in expiration key at sanctum.php
    
    add docker-compose.yml file
    
    create LoginUserController.php to login user
    
    create LoginUserRequest.php to validate login user data
    
    [skip ci]

---
+++ app/Http/Controllers/Auth/LoginUserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginUserRequest;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class LoginUserController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(LoginUserRequest $request)
+    {
+        if (!Auth::attempt($request->validated())) {
+            return response()->json(['message' => __('auth.failed')], Response::HTTP_BAD_REQUEST);
+        }
+
+        $token = $request->user()->createToken(
+            'personal token',
+            ['*'],
+            now()->addMinutes(config('sanctum.expiration')))->plainTextToken;
+
+        return response()->json(compact('token'));
+    }
+}
