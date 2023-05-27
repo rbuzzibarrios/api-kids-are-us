2c3048148f0c8dbe2e56d9e2d591b13bbe980d2f
Rodolfo <42749139+rbuzzibarrios@users.noreply.github.com>
2023-05-27 15:52:47 -0400

    Install and configure laratrust package (#2)
    * install and configure laratrust package
    
    add laratrus migration at 2023_05_27_142730_laratrust_setup_tables.php
    
    [skip ci]
    
    * call LaratrustSeeder at DatabaseSeeder.php
    
    create role structure and map permissions in laratrust_seeder.php
    
    add LaratrustSeeder.php seeder
    
    * fix pint errors and fix phpstan errors
    
    * set db port and redis port whe run db seed in workflow-build.yml
    
    * use cache v3 in laravel.yml
    
    upload logs in workflow-build.yml
    
    * add php artisan optimize in workflow-build.yml
    
    * remove redis service from workflow-build.yml
    
    * install predis
    
    * use redis in redis_host key at env.ci
    use localhost in db_host key at .env.ci
    
    remove env vars in seed database application at workflow-build.yml
    
    * use 127.0.0.1 in db_host key at .env.ci
    
    * use 127.0.0.1 in REDIS_HOST key at .env.ci
    
    * use redis in REDIS_HOST key at .env.ci
    update ports to 6379
    
    * use localhost in REDIS_HOST key at .env.ci
    
    * rollback run artisan serve command with &
    
    * fix errors in laravel.yml

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(LaratrustSeeder::class);
     }
 }
